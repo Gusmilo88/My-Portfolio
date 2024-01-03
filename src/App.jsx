6ce1b184a975b1b34728367a3feb3e95c8ba2839
Gus <gus_eze1988@hotmail.com>
2024-01-02 22:23:07 -0300

    estructura inicial

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Spline from '@splinetool/react-spline'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Hola</h1>
+      <Spline scene="https://prod.spline.design/hSE6LJD9LiS-1xgS/scene.splinecode" />
+
+    </>
+  )
+}
+
+export default App
