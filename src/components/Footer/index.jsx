cb8603c2d7b2bc9324fc1658a93711033ee490fd
Gus <gus_eze1988@hotmail.com>
2024-01-15 20:29:56 -0300

    footer terminado

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -9,25 +9,16 @@ const Footer = () => {
   <footer role='footer' className="flex flex-col items-center text-center dark:bg-zinc-900">
     <div className="container pt-9">
       <div className="mb-9 flex justify-center">
-        <Link to={"https://www.facebook.com/"} target="_blank" className="mr-9">
+        <Link to={"https://www.facebook.com/GusPollito/"} target="_blank" className="mr-9">
           <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" viewBox="0 0 24 24">
             <linearGradient id="gradiente" x1="0%" y1="0%" x2="100%" y2="0%">
-            <stop offset="0%" style={{ stopColor: '#d53369' }} />
-              <stop offset="100%" style={{stopColor: '#daae51'}} />
+            <stop offset="0%" style={{ stopColor: '#00bcd4' }} />
+              <stop offset="100%" style={{stopColor: '#8e44ad'}} />
             </linearGradient>
             <path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385" fill="url(#gradiente)" />
           </svg>
         </Link>
-        <Link to={"https://twitter.com/home"}  target="_blank" className="mr-9 text-neutral-800 dark:text-neutral-200">
-          <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" fill="currentColor" viewBox="0 0 24 24">
-            <linearGradient id="gradiente" x1="0%" y1="0%" x2="100%" y2="0%">
-              <stop offset="0%" style={{stopColor: '#d53369'}} />
-              <stop offset="100%" style={{stopColor: '#daae51'}} />
-            </linearGradient>
-            <path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z" fill="url(#gradiente)" />
-          </svg>
-        </Link>
-        <Link to={"https://www.google.com/intl/es-419/gmail/about/"}  target="_blank" className="mr-9 text-neutral-800 dark:text-neutral-200">
+        <Link to={"mailto:gus_eze1988@hotmail.com"}  target="_blank" className="mr-9 text-neutral-800 dark:text-neutral-200">
           <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="currentColor" viewBox="0 0 24 24">
             <linearGradient id="gradiente" x1="0%" y1="0%" x2="100%" y2="0%">
               <stop offset="0%" style={{stopColor: '#d53369'}} />
@@ -36,7 +27,7 @@ const Footer = () => {
             <path d="M7 11v2.4h3.97c-.16 1.029-1.2 3.02-3.97 3.02-2.39 0-4.34-1.979-4.34-4.42 0-2.44 1.95-4.42 4.34-4.42 1.36 0 2.27.58 2.79 1.08l1.9-1.83c-1.22-1.14-2.8-1.83-4.69-1.83-3.87 0-7 3.13-7 7s3.13 7 7 7c4.04 0 6.721-2.84 6.721-6.84 0-.46-.051-.81-.111-1.16h-6.61zm0 0 17 2h-3v3h-2v-3h-3v-2h3v-3h2v3h3v2z" fillRule="evenodd" clipRule="evenodd" fill="url(#gradiente)" />
           </svg>
         </Link>
-        <Link to={"https://www.instagram.com/"}  target="_blank" className="mr-9 text-neutral-800 dark:text-neutral-200">
+        <Link to={"https://www.instagram.com/gus_romero88/"}  target="_blank" className="mr-9 text-neutral-800 dark:text-neutral-200">
           <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" fill="currentColor" viewBox="0 0 24 24">
             <linearGradient id="gradiente" x1="0%" y1="0%" x2="100%" y2="0%">
               <stop offset="0%" style={{stopColor: '#d53369'}} />
@@ -45,7 +36,7 @@ const Footer = () => {
             <path d="M12 2.163c3.204 0 3.584.012 4.85.07 3.252.148 4.771 1.691 4.919 4.919.058 1.265.069 1.645.069 4.849 0 3.205-.012 3.584-.069 4.849-.149 3.225-1.664 4.771-4.919 4.919-1.266.058-1.644.07-4.85.07-3.204 0-3.584-.012-4.849-.07-3.26-.149-4.771-1.699-4.919-4.92-.058-1.265-.07-1.644-.07-4.849 0-3.204.013-3.583.07-4.849.149-3.227 1.664-4.771 4.919-4.919 1.266-.057 1.645-.069 4.849-.069zm0-2.163c-3.259 0-3.667.014-4.947.072-4.358.2-6.78 2.618-6.98 6.98-.059 1.281-.073 1.689-.073 4.948 0 3.259.014 3.668.072 4.948.2 4.358 2.618 6.78 6.98 6.98 1.281.058 1.689.072 4.948.072 3.259 0 3.668-.014 4.948-.072 4.354-.2 6.782-2.618 6.979-6.98.059-1.28.073-1.689.073-4.948 0-3.259-.014-3.667-.072-4.947-.196-4.354-2.617-6.78-6.979-6.98-1.281-.059-1.69-.073-4.949-.073zm0 5.838c-3.403 0-6.162 2.759-6.162 6.162s2.759 6.163 6.162 6.163 6.162-2.759 6.162-6.163c0-3.403-2.759-6.162-6.162-6.162zm0 10.162c-2.209 0-4-1.79-4-4 0-2.209 1.791-4 4-4s4 1.791 4 4c0 2.21-1.791 4-4 4zm6.406-11.845c-.796 0-1.441.645-1.441 1.44s.645 1.44 1.441 1.44c.795 0 1.439-.645 1.439-1.44s-.644-1.44-1.439-1.44z" fill="url(#gradiente)" />
           </svg>
         </Link>
-        <Link to={"https://www.linkedin.com/"} target="_blank" className="mr-9 text-neutral-800 dark:text-neutral-200">
+        <Link to={"https://www.linkedin.com/in/gus-romero/"} target="_blank" className="mr-9 text-neutral-800 dark:text-neutral-200">
           <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" fill="currentColor" viewBox="0 0 24 24">
             <linearGradient id="gradiente" x1="0%" y1="0%" x2="100%" y2="0%">
               <stop offset="0%" style={{stopColor: '#d53369'}} />
@@ -66,8 +57,7 @@ const Footer = () => {
       </div>
     </div>
     <div id="footer_copyright" className="w-full bg-neutral-300 p-4 text-center text-neutral-700 dark:bg-zinc-900 dark:text-neutral-200">
-      © 2024 Copyright:
-      <Link className="text-neutral-50 dark:text-neutral-50" to={"/"} target="_blank"> By Gus Romero</Link>
+      © 2024 Copyright: All rights reserved by Gus Romero.
     </div>
   </footer>
 </div>
